3111854c9260108874c4895c65b8fc912b4ef921
SUVAJIT <suvajit.me@gmail.com>
2021-01-13 21:55:33 +0530

    feat(Logger): add Logger

---
+++ src/bot/util/Logger.ts
@@ -0,0 +1,48 @@
+import moment from 'moment';
+import chalk from 'chalk';
+import util from 'util';
+
+const COLORS: { [key: string]: string } = {
+	debug: 'yellow',
+	info: 'cyan',
+	warn: 'magenta',
+	error: 'red'
+};
+
+const TAGS: { [key: string]: string } = {
+	debug: '[DEBUG]',
+	info: '[INFO ]',
+	warn: '[WARN ]',
+	error: '[ERROR]'
+};
+
+export default class Logger {
+	public debug(message: string | any, { label }: { label?: string }) {
+		return (this.constructor as typeof Logger).write(message, { label, tag: 'debug' });
+	}
+
+	public info(message: string | any, { label }: { label?: string }) {
+		return (this.constructor as typeof Logger).write(message, { label, tag: 'info' });
+	}
+
+	public error(message: string | any, { label }: { label?: string }) {
+		return (this.constructor as typeof Logger).write(message, { error: true, label, tag: 'error' });
+	}
+
+	public warn(message: string | any, { label }: { label?: string }) {
+		return (this.constructor as typeof Logger).write(message, { label, tag: 'warn' });
+	}
+
+	private static write(message: string | any, { error, label, tag }: { error?: boolean; label?: string; tag: string }) {
+		const timestamp = chalk.cyan(moment().utcOffset('+05:30').format('DD-MM-YYYY kk:mm:ss'));
+		const content = this.clean(message);
+		const stream = error ? process.stderr : process.stdout;
+		const color = COLORS[tag] as 'red' | 'cyan' | 'yellow' | 'magenta';
+		stream.write(`[${timestamp}] [SHARD 0] ${chalk[color].bold(TAGS[tag])} » ${label ? `[${label}] » ` : ''}${content}\n`);
+	}
+
+	private static clean(message: string | any) {
+		if (typeof message === 'string') return message;
+		return util.inspect(message, { depth: Infinity });
+	}
+}
