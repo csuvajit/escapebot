2d3a8a0043b0b2a0f6ac1990a9292c54e74e40fb
SUVAJIT <suvajit.me@gmail.com>
2021-01-13 22:05:56 +0530

    feat(Command): add help and prefix commands

---
+++ src/bot/commands/util/Help.ts
@@ -0,0 +1,50 @@
+import { Command, PrefixSupplier } from 'discord-akairo';
+import { Message, MessageEmbed } from 'discord.js';
+
+export default class HelpCommand extends Command {
+	public constructor() {
+		super('help', {
+			aliases: ['help'],
+			category: 'util',
+			typing: true,
+			clientPermissions: ['EMBED_LINKS'],
+			args: [
+				{
+					id: 'command',
+					type: 'commandAlias'
+				}
+			]
+		});
+	}
+
+	public async exec(message: Message, { command }: { command: Command | null }) {
+		const prefix = (this.handler.prefix as PrefixSupplier)(message) as string;
+		if (!command) {
+			const embed = new MessageEmbed()
+				.setAuthor('Command List', this.client.user!.displayAvatarURL());
+			for (const category of this.handler.categories.values()) {
+				embed.addField(
+					category.id.replace(/\b(\w)/g, char => char.toUpperCase()),
+					category.filter(cmd => cmd.aliases.length > 0).map(cmd => `\`${cmd.aliases[0]}\``).join(', ')
+				);
+			}
+
+			return message.util!.send({ embed });
+		}
+
+		const embed = new MessageEmbed()
+			.setTitle(`\`${prefix}${command.aliases[0]} ${command.description.usage as string || ''}\``)
+			.addField('Description', command.description.content || 'No description');
+
+		if (command.aliases.length > 1) embed.addField('Aliases', `\`${command.aliases.join('` `')}\``, true);
+		if (command.description.examples?.length) {
+			embed.addField(
+				'Examples',
+				`\`${prefix}${command.aliases[0]} ${command.description.examples.join(`\`\n\`${command.aliases[0]} `) as string}\``,
+				true
+			);
+		}
+
+		return message.util!.send({ embed });
+	}
+}
---
+++ src/bot/commands/util/Prefix.ts
@@ -0,0 +1,47 @@
+import { Argument, Command, PrefixSupplier } from 'discord-akairo';
+import { Message } from 'discord.js';
+
+export default class PrefixCommand extends Command {
+	public constructor() {
+		super('prefix', {
+			aliases: ['prefix'],
+			category: 'config',
+			channel: 'guild',
+			quoted: false,
+			description: {
+				content: 'Displays or changes the prefix.',
+				usage: '<prefix>',
+				examples: ['!', '?']
+			},
+			args: [
+				{
+					id: 'prefix',
+					type: Argument.validate('string', (message, prefix) => !/\s/.test(prefix) && prefix.length <= 3)
+				}
+			]
+		});
+	}
+
+	// @ts-ignore
+	public regex() {
+		return new RegExp(`^<@!?(${this.client.user!.id})>$`, 'i');
+	}
+
+	public exec(message: Message, { prefix }: { prefix: string | null }) {
+		if (/^<@!?(\d+)>$/.test(message.content) && !message.mentions.has(this.client.user!.id)) return;
+
+		if (!prefix) {
+			return message.util!.send(`The current prefix for this server is \`${(this.handler.prefix as PrefixSupplier)(message) as string}\``);
+		}
+
+		if (prefix && !message.member!.permissions.has('MANAGE_GUILD')) {
+			return message.util!.send([
+				`The current prefix for this server is \`${(this.handler.prefix as PrefixSupplier)(message) as string}\``,
+				'You are missing `Manage Server` to change the prefix.'
+			]);
+		}
+
+		this.client.settings.set(message.guild!, 'prefix', prefix);
+		return message.util!.send(`Prefix has been set to \`${prefix}\``);
+	}
+}
