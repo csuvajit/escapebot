e12e6361a7ca8da0fa40495355d62bb58c0a2b05
Suvajit <suvajit.me@gmail.com>
2021-03-04 21:18:47 +0530

    Implement Sentry SDK

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@ import Env from 'dotenv';
 Env.config();
 
 import Client from './bot/struct/Client';
-
+import * as Sentry from '@sentry/node';
 const client = new Client();
+
+if (process.env.SENTRY) {
+	Sentry.init({
+		dsn: process.env.SENTRY,
+		environment: process.env.NODE_ENV
+	});
+}
 
 client.on('error', error => client.logger.error(error, { label: 'ERROR' }));
 client.on('warn', error => client.logger.warn(error, { label: 'WARN' }));
